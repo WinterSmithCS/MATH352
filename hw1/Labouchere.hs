2158f563dc289fcf97efdfa2c46247f88d3270b9
anthonybrice <anthony.brice423@myci.csuci.edu>
2014-09-01 15:50:12 -0700

    updated Labouchere.hs

--- hw1/Labouchere.hs
+++ hw1/Labouchere.hs
@@ -1,24 +1,28 @@
 -- file Labouchere.hs
 -- A program to simulate the Labouchere system
 
-import System.Random (randomRIO)
-import System.IO.Unsafe (unsafePerformIO)
+import System.Random (randomR, getStdGen, StdGen)
 
-pick :: [a] -> IO a
-pick xs = randomRIO (0, length xs - 1) >>= return . (xs !!)
+labouchere :: Num a => [a] -> IO a
+labouchere xs = do
+  gen <- getStdGen
+  labouchere' xs 0 gen
 
-labouchere :: Num a => [a] -> a
-labouchere xs = labouchere' xs 0
+labouchere' :: Num a => [a] -> a -> StdGen -> IO a
+labouchere' [] acc gen = return acc
+labouchere' (x:[]) acc gen = do
+  let (randNumber, newGen) = randomR (1,37) gen :: (Int, StdGen)
+  if roulette!!randNumber `elem` red
+    then return $ acc + x
+    else labouchere' (x:[x]) (acc - x) newGen
+labouchere' (x:xs) acc gen = do
+  let (randNumber, newGen) = randomR (1,37) gen :: (Int, StdGen)
+  if roulette!!randNumber `elem` red
+    then labouchere' (init xs) (acc + x + last xs) newGen
+    else labouchere' ((x:xs) ++ [x + last xs]) (acc - x - last xs) newGen
 
 roulette = [0..37]
 
-red = [1,3,5,7,9,12,14,16,18,19,21,23,25,27,30,32,34,26]
+red = [1,3,5,7,9,12,14,16,18,19,21,23,25,27,30,32,34,36]
 
-labouchere' :: Num a => [a] -> a -> a
-labouchere' [] acc     = acc
-labouchere' (x:[]) acc = if unsafePerformIO (pick roulette) `elem` red
-                         then acc + x
-                         else labouchere' (x:[x]) (acc - x)
-labouchere' (x:xs) acc = if unsafePerformIO (pick roulette) `elem` red
-                         then labouchere' (init xs) (acc + x + last xs)
-                         else labouchere' ((x:xs) ++ [x + last xs]) (acc - x - last xs)
+
