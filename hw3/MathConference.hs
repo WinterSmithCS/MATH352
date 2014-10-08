9112b15923f86abb75f62487561ade192e40b2ed
anthonybrice <anthony.brice423@myci.csuci.edu>
2014-10-07 22:10:21 -0700

    foo

--- hw3/MathConference.hs
+++ hw3/MathConference.hs
@@ -8,7 +8,7 @@ import Data.Graph.Inductive.PatriciaTree (Gr)
 import Data.Graph.Inductive.Graph (mkGraph, Node, edges)
 import Data.Graph.Inductive.Basic (undir)
 
-seatingArrangement :: (Enum a, Enum b, Eq b, Num a, Num b) => [Node] -> Gr a b
+seatingArrangement :: [Node] -> Gr Int Int
 seatingArrangement vs = undir $ mkGraph lnodes ledges
   where ledges = take (length vs) $ zip3 (cycle vs) (tail $ cycle vs) [1..]
         lnodes = take (length vs) $ zip vs [1..]
@@ -19,13 +19,13 @@ noProfessorsAdjacentTwice xs ys = null [ x | x <- edges xs, y <- edges ys, x == 
 main :: IO ()
 main = do
   args <- getArgs
-  let (n, iters) = (read (args!!0)::Int, read (args!!1)::Int)
+  let (n, iters) = (read (head args)::Int, read (args!!1)::Int)
   gens <- replicateM (2*iters) newStdGen
   let lists = map (shuffle' [1..n] n) gens :: [[Int]]
       arrangementGraphs = map seatingArrangement lists
       pairedMeals = splitAt iters arrangementGraphs
       sims = length $ filter (== True) $ uncurry (zipWith noProfessorsAdjacentTwice) pairedMeals
-      prob = fromIntegral sims / fromIntegral iters
+      prob = fromIntegral sims / fromIntegral iters :: Double
   putStrLn $ "We estimate a " ++ show prob ++ " probability."
 
 --  LocalWords:  MathConference hs
